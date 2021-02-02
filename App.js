35ff4320ec947c7978de6d79447493ffcf7ffb94
sanjeevakumar-star <77141876+sanjeevakumar-star@users.noreply.github.com>
2021-02-01 22:08:27 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import app from './App.css';
+import Navbar from'./components/Navbar';
+import Banner from './components/Banner';
+//import Product from './components/Products';
+
+
+function App(){
+  return(<div>
+    
+    <Navbar/>
+    <Banner/>
+    
+  
+  </div>)
+
+}
+export default App;
